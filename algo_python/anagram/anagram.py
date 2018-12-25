3a82a9e68caacf36fb0e32e19bf2676c3b1cc6d4
STARBUCKS\aodendaa <aodendaal.direct@gmail.com>
2018-12-24 21:05:11 -0800

    Added sort solution and buggy solution from the website

--- algo_python/anagram/anagram.py
+++ algo_python/anagram/anagram.py
@@ -10,3 +10,49 @@ def checking_off(s1,s2):
             anagram = False
 
     return anagram
+
+
+def sorting(s1, s2):
+
+    list_1 = list(s1.lower())
+    list_1.sort()
+    list_2 = list(s2.lower())
+    list_2.sort()
+
+    return list_1 == list_2
+
+
+def anagramSolution1(s1,s2):
+    """
+    Solution given in
+    https://runestone.academy/runestone/static/pythonds/AlgorithmAnalysis/AnAnagramDetectionExample.html?lastPosition=1423.4666748046875
+    """
+    #AO Moved True assertion to top of function
+    stillOK = True
+
+    if len(s1) != len(s2):
+        stillOK = False
+
+    alist = list(s2)
+
+    pos1 = 0
+    #Lenght check will never work in original solution
+    #AO stillOK = True
+
+    while pos1 < len(s1) and stillOK:
+        pos2 = 0
+        found = False
+        while pos2 < len(alist) and not found:
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+
+        if found:
+            alist[pos2] = None
+        else:
+            stillOK = False
+
+        pos1 = pos1 + 1
+
+    return stillOK
--- algo_python/anagram/test_anagram.py
+++ algo_python/anagram/test_anagram.py
@@ -2,7 +2,7 @@ import anagram
 import unittest
 
 
-class TestAnagram(unittest.TestCase):
+class TestAnagramCheckingOff(unittest.TestCase):
     def test_EqualLength(self):
         self.assertEqual(anagram.checking_off('Hello', 'Helloo'), False)
 
@@ -11,6 +11,35 @@ class TestAnagram(unittest.TestCase):
 
     def test_Not_Anagram(self):
         self.assertEqual(anagram.checking_off('Heart', 'aaaaa'), False)
+
+
+class TestAnagramSorting(unittest.TestCase):
+    def test_EqualLength(self):
+        self.assertEqual(anagram.sorting('Hello', 'Helloo'), False)
+
+    def test_Anagram(self):
+        self.assertEqual(anagram.sorting('Heart', 'Earth'), True)
+
+    def test_Not_Anagram(self):
+        self.assertEqual(anagram.sorting('Heart', 'aaaaa'), False)
+
+    def test_Length(self):
+        self.assertEqual(anagram.sorting('aa', 'aaa'), False)
+
+
+class TestAnagramGivenSolution1(unittest.TestCase):
+    def test_EqualLength(self):
+        self.assertEqual(anagram.anagramSolution1('Hello', 'Helloo'), False)
+
+    def test_Anagram(self):
+        self.assertEqual(anagram.anagramSolution1('Heart', 'Earth'), True)
+
+    def test_Not_Anagram(self):
+        self.assertEqual(anagram.anagramSolution1('Heart', 'aaaaa'), False)
+
+    def test_Length(self):
+        self.assertEqual(anagram.anagramSolution1('aa', 'aaa'), False)
+
 
 if __name__ == '__main__':
     unittest.main()
