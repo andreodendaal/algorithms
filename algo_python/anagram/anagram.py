3d5692ac52efade33c7a1ba4daff3f22d436f41e
andre odendaal <aodendaal.direct@gmail.com>
2018-12-27 02:21:47 -0800

    anagram algo: count and compare

--- algo_python/anagram/anagram.py
+++ algo_python/anagram/anagram.py
@@ -1,3 +1,4 @@
+import string
 def checking_off(s1,s2):
     anagram = True
     checklist = list(s2.lower())
@@ -56,3 +57,60 @@ def anagramSolution1(s1,s2):
         pos1 = pos1 + 1
 
     return stillOK
+
+def anagramSolution4(s1,s2):
+    """Count and compare
+    From: https://runestone.academy/runestone/static/pythonds/AlgorithmAnalysis/AnAnagramDetectionExample.html#solution-2-sort-and-compare
+
+    >>> anagramSolution4('azzle','zleaz')
+    True
+
+    !does not account for uppercase
+    """
+    c1 = [0]*26
+    c2 = [0]*26
+
+    for i in range(len(s1)):
+        val1 = ord(s1[i])
+        val2 = ord('a')
+        pos = ord(s1[i])-ord('a')
+        c1[pos] = c1[pos] + 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i])-ord('a')
+        c2[pos] = c2[pos] + 1
+
+    j = 0
+    stillOK = True
+    while j<26 and stillOK:
+        if c1[j]==c2[j]:
+            j = j + 1
+        else:
+            stillOK = False
+
+    return stillOK
+
+def count_and_compare(s1, s2):
+    """Count and compare
+    >>> count_and_compare('azZle','zleaz')
+    True
+    >>> count_and_compare('azble','zleaz')
+    False
+
+    """
+
+    ref_dict = dict((key, ctr) for ctr, key in enumerate(string.ascii_lowercase, start=1))
+    total_s1 = 0
+    total_s2 = 0
+
+    for i in s1.lower():
+        total_s1 += ref_dict[i]
+
+    for i in s2.lower():
+        total_s2 += ref_dict[i]
+
+    return total_s1 == total_s2
+
+
+
+
