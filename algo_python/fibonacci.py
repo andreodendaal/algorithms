50efa4b95e2d177ec7a03da68123b712b6302ed9
andre odendaal <aodendaal.direct@gmail.com>
2018-12-27 17:50:16 -0800

    Add to Fibanocci algo

--- algo_python/fibonacci.py
+++ algo_python/fibonacci.py
@@ -14,26 +14,38 @@ def fibbonacci(p_val, p_range):
     >>> fibbonacci(37, 0)
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
+    >>> fibbonacci(55, 0)
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+    >>> fibbonacci(1, 0)
+    [0, 1, 1]
+
+    >>> fibbonacci(0, 0)
+    [0]
+
     """
+
     # initialize the sequence
     fib_list = [0, 1]
 
     # calculate the Fibonacci sequence for a range of numbers
-    if p_val == 0:
+    if p_range != 0:
 
         for i in range(p_range-2):
             fib_list.append(fib_list[i] + fib_list[-1])
 
     # calculate the Fibonacci sequence up to a certain value
-    else:
+    elif p_val != 0:
 
         ctr = 0
-        while fib_list[-1] <= p_val:
+        while fib_list[ctr] < p_val:
 
-            if (fib_list[ctr] + fib_list[-1]) > p_val:
+            if (fib_list[ctr] + fib_list[ctr+1]) > p_val:
                 break
 
             fib_list.append(fib_list[ctr] + fib_list[-1])
             ctr += 1
+    else:
+        return [0]
 
     return fib_list
